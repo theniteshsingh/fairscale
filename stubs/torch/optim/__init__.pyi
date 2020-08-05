c4e019587e655b264df5485e96dd7e775d92e610
Jun Ru Anderson <andersonic@fb.com>
2020-08-05 16:31:16 -0700

    stubs again

--- stubs/torch/optim/__init__.pyi
+++ stubs/torch/optim/__init__.pyi
@@ -4,6 +4,7 @@ from .sgd import SGD as SGD
 from .adam import Adam as Adam
 from . import lr_scheduler as lr_scheduler
 from .optimizer import Optimizer as Optimizer
+from .grad_scaler import GradScaler
 #MODIFIED BY TORCHGPIPE
 from .rmsprop import RMSprop as RMSprop
 #END
